e4e10b99393841e444fc540afdc11a832811e932
ErnestoAzziorri2021 <ernestoazziorri@gmail.com>
2022-07-07 22:22:22 -0300

    dockerfile
    cambios en dockerfile

--- src/main/java/uy/edu/ort/devops/shippingserviceexample/ShippingServiceExampleApplication.java
+++ src/main/java/uy/edu/ort/devops/shippingserviceexample/ShippingServiceExampleApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShippingServiceExampleApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Shipping Url: " + args[0]);
+
 		SpringApplication.run(ShippingServiceExampleApplication.class, args);
 	}
 
